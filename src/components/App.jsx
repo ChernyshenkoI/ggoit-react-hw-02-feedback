dc1baf9fe3e3a0d0556ba650f2e2537fcac61052
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-02-17 17:05:10 +0200

    final version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template it is for the test 
-    </div>
-  );
-};
+import Feedback from "./Feedback/Feedback";
+
+
+export const App = ()=>{
+  return <Feedback/>
+}
+
+
+
